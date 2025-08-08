ee0b33649fb3c8676af7f5056853b585353ad812
karensantos09 <karen.c.santos8@aluno.senai.br>
2025-08-08 16:35:41 -0300

    Adiciona funcionalidade inicial de gerenciamento de tarefas
    - Alterado `_Layout.cshtml` para ajustar idioma, formatação e
      adicionar link para "Lista de Tarefas" no menu de navegação.
    - Criado `TarefasController` com ação `Index` para exibir tarefas.
    - Adicionado modelo `Tarefa` com propriedades básicas.
    - Criada view `Index.cshtml` para listar tarefas com título e cabeçalho.

---
+++ APPTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APPTarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
